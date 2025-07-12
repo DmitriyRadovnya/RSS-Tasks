597fa3d78a12fab41e615645600c00ea7feeebad
DmitriyRadovnya <dima.radovnya@gmail.com>
2025-07-12 18:17:09 +0300

    feat: implement cards layout & add card styles

--- class-components/src/components/main/card-list/card/card.tsx
+++ class-components/src/components/main/card-list/card/card.tsx
@@ -10,17 +10,48 @@ export default class CardList extends React.Component<CardProps> {
   render() {
     const {
       name,
-      base_experience: baseExperience,
+      base_experience: baseExp,
       sprites: { front_default: cardImageUrl },
+      abilities,
+      stats,
     } = this.props.pokemonInfo;
     return (
-      <li className="card">
-        <img src={cardImageUrl} alt="Pokemon Image" />
-        <div>
-          <h3>Name: {name}</h3>
-          <span>Base exp: {baseExperience}</span>
+      <div className="cardStyle">
+        <div style={{ width: '30%' }}>
+          <img src={cardImageUrl} alt={name} className="imageStyle" />
+          <h2 style={{ margin: 0 }}>{name}</h2>
         </div>
-      </li>
+        <div className="infoStyle">
+          <p style={{ margin: '8px 0' }}>Base experience: {baseExp}</p>
+
+          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <div>
+              <h4 style={{ margin: 0 }} className="titleStyle">
+                Stats
+              </h4>
+              <ul className="listStyle">
+                {stats.map((statObject, index) => (
+                  <li style={{ textAlign: 'start' }} key={index}>
+                    {statObject.stat.name}: {statObject.base_stat}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h4 style={{ margin: 0 }} className="titleStyle">
+                Abilities
+              </h4>
+              <ul className="listStyle">
+                {abilities.map((abilityObject, index) => (
+                  <li style={{ textAlign: 'start' }} key={index}>
+                    {abilityObject.ability?.name || 'unknown ability'}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
