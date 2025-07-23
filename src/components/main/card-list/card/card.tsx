c8f0e2892a00ea5fefbcfede35a20e6e8cdede33
DmitriyRadovnya <dima.radovnya@gmail.com>
2025-07-23 11:36:09 +0300

    refactor: Card component to func

--- src/components/main/card-list/card/card.tsx
+++ src/components/main/card-list/card/card.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './card.css';
 import type { PokemonDetails } from '../../../../interfaces/interfaces';
 
@@ -6,52 +5,51 @@ interface CardProps {
   pokemonInfo: PokemonDetails;
 }
 
-export default class Card extends React.Component<CardProps> {
-  render() {
-    const {
-      name,
-      base_experience: baseExp,
-      sprites: { front_default: cardImageUrl },
-      abilities,
-      stats,
-    } = this.props.pokemonInfo;
-    return (
-      <div className="cardStyle">
-        <div style={{ width: '30%' }}>
-          <img src={cardImageUrl} alt={name} className="imageStyle" />
-          <h2 style={{ margin: 0 }}>{name}</h2>
-        </div>
-        <div className="infoStyle">
-          <p style={{ margin: '8px 0' }}>Base experience: {baseExp}</p>
+export default function Card(props: CardProps) {
+  const {
+    name,
+    base_experience: baseExp,
+    sprites: { front_default: cardImageUrl },
+    abilities,
+    stats,
+  } = props.pokemonInfo;
 
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <div>
-              <h4 style={{ margin: 0 }} className="titleStyle">
-                Stats
-              </h4>
-              <ul className="listStyle">
-                {stats.map((statObject, index) => (
-                  <li style={{ textAlign: 'start' }} key={index}>
-                    {statObject.stat.name}: {statObject.base_stat}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <h4 style={{ margin: 0 }} className="titleStyle">
-                Abilities
-              </h4>
-              <ul className="listStyle">
-                {abilities.map((abilityObject, index) => (
-                  <li style={{ textAlign: 'start' }} key={index}>
-                    {abilityObject.ability.name || 'unknown ability'}
-                  </li>
-                ))}
-              </ul>
-            </div>
+  return (
+    <div className="cardStyle">
+      <div style={{ width: '30%' }}>
+        <img src={cardImageUrl} alt={name} className="imageStyle" />
+        <h2 style={{ margin: 0 }}>{name}</h2>
+      </div>
+      <div className="infoStyle">
+        <p style={{ margin: '8px 0' }}>Base experience: {baseExp}</p>
+
+        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <div>
+            <h4 style={{ margin: 0 }} className="titleStyle">
+              Stats
+            </h4>
+            <ul className="listStyle">
+              {stats.map((statObject, index) => (
+                <li style={{ textAlign: 'start' }} key={index}>
+                  {statObject.stat.name}: {statObject.base_stat}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h4 style={{ margin: 0 }} className="titleStyle">
+              Abilities
+            </h4>
+            <ul className="listStyle">
+              {abilities.map((abilityObject, index) => (
+                <li style={{ textAlign: 'start' }} key={index}>
+                  {abilityObject.ability.name || 'unknown ability'}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
