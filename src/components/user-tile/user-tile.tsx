a1da300958cea0536e78db26c9a927f0a03e4e43
DmitriyRadovnya <dima.radovnya@gmail.com>
2025-08-24 18:14:44 +0300

    feat: implement avatar conversion for user-tile

--- src/components/user-tile/user-tile.tsx
+++ src/components/user-tile/user-tile.tsx
@@ -1,13 +1,31 @@
-import type { FC } from 'react';
+import './user-tile.css';
+import { useEffect, useState, type FC } from 'react';
 import type { IFormData } from '../../interfaces/interfaces';
+import { getImageSrc } from './user-tile.lib';
 
 interface IUserTileProps {
   data: IFormData;
 }
 
-export const UserTile: FC<IUserTileProps> = ({ data: { name } }) => {
+export const UserTile: FC<IUserTileProps> = ({ data: { name, avatar } }) => {
+  const [highlight, setHighlight] = useState(true);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => setHighlight(false), 3000);
+    return () => clearTimeout(timerId);
+  }, []);
+
+  const imageSrc = avatar ? getImageSrc(avatar) : '';
+
   return (
-    <div className="user-tile">
+    <div className={`user-tile ${highlight ? 'highlight' : ''}`}>
+      {imageSrc && (
+        <img
+          src={imageSrc}
+          alt={`${name}'s avatar`}
+          style={{ maxWidth: '100px', maxHeight: '100px' }}
+        />
+      )}
       <p>{name}</p>
     </div>
   );
