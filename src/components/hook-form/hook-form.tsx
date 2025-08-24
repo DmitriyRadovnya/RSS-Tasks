e57213743eee69a79a0ad1dc4b365ae541d57501
DmitriyRadovnya <dima.radovnya@gmail.com>
2025-08-24 09:42:37 +0300

    feat: implement basic hook-form

---
+++ src/components/hook-form/hook-form.tsx
@@ -0,0 +1,108 @@
+import { useForm } from 'react-hook-form';
+
+interface IFormData {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  gender: 'male' | 'female';
+  terms: boolean;
+  country: string;
+}
+
+export const HookForm = () => {
+  const { register, handleSubmit } = useForm<IFormData>({
+    defaultValues: {
+      name: '',
+      age: 18,
+      email: '',
+      password: '',
+      confirmPassword: '',
+      gender: 'male',
+      terms: false,
+      country: '',
+    },
+  });
+
+  const onSubmit = (data: IFormData) => {
+    console.log(data);
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <h1></h1>
+      <div className="form-field">
+        <label htmlFor="name">Name:</label>
+        <input className="input_name" id="name" {...register('name')} />
+      </div>
+
+      <div className="form-field">
+        <label htmlFor="age">Age:</label>
+        <input
+          className="input_age"
+          type="number"
+          id="age"
+          {...register('age', { valueAsNumber: true })}
+        />
+      </div>
+      <div className="form-field">
+        <label htmlFor="email">Email:</label>
+        <input
+          className="input_email"
+          type="email"
+          id="email"
+          {...register('email')}
+        />
+      </div>
+
+      <div className="form-field">
+        <label htmlFor="password">Password:</label>
+        <input
+          className="input_password"
+          type="password"
+          id="password"
+          {...register('password')}
+        />
+      </div>
+      <div className="form-field">
+        <label htmlFor="confirm-password">Confirm password:</label>
+        <input
+          className="input_password"
+          type="password"
+          id="confirm-password"
+          {...register('confirmPassword')}
+        />
+      </div>
+
+      <div className="form-field">
+        <label htmlFor="gender"></label>
+        <select id="gender" {...register('gender')}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+
+      <div className="form-field">
+        <label htmlFor="avatar">Select avatar</label>
+        <input
+          className="input_avatar"
+          type="file"
+          accept="image/png, image/jpeg"
+          id="avatar"
+        />
+      </div>
+
+      <label htmlFor="terms">
+        <input
+          className="checkbox_terms"
+          type="checkbox"
+          id="terms"
+          {...register('terms')}
+        />
+        Accept Terms and Conditions
+      </label>
+      <button>Submit</button>
+    </form>
+  );
+};
